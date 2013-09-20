b01c68a71b3e2935cf3563235206a0770ba90a34
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-09-20 17:41:35 -0500

    Water drive
    agregue este nuevo producto

---
+++ water-drive.php
@@ -0,0 +1,93 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$nombre"?> | SERIE WATER DRIVE</title>
+ 		<meta name="description" content="WATER DRIVE es un variador de velocidad enfriado por agua, robusto e intuitivo que permite de una manera moderna y funcional el diseño de sistemas de bombeo de presión constante con un alta eficiencia energética.">
+ 		<meta name="keywords" content="WATER, DRIVE, variador, velocidad, enfriado por agua, robusto, intuitivo, moderno, funcional, sistemas, bombeo, presión constante, eficiencia, energética">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 	<?php include("header.php"); ?>
+ 	<section id="ax" class="active">
+ 	<img src="images/WD-ficha-comercial.jpg" alt="ficha comercial">
+ 	<div class="cien">
+ 	<h2 class="headline fdrive">SERIE WATER DRIVE</h2>
+ 	<span>VARIADOR DE FRECUENCIA PARA SUMINISTRO DE AGUA A PRESIÓN CONSTANTE</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h3>ESPECIFICACIONES</h3>
+	 		<article>
+		 		<p>WATER DRIVE es un variador de velocidad enfriado por agua, robusto e intuitivo que permite de una manera moderna y funcional el diseño de sistemas de bombeo de presión constante con un alta eficiencia energética.</p>
+		 		<h4>Aplicaciones</h4>
+				<ul>
+					<li>Viviendas					   </li>
+					<li>Edificios					   </li>
+					<li>Casas de campo				   </li>
+					<li>Suminisitro de agua a viviendas</li>
+					<li>Oficinas					   </li>
+					<li>Centros comerciales			   </li>
+					<li>Hoteles						   </li>
+					<li>Restaurantes				   </li>
+				</ul>
+				<h4>VENTAJAS</h4>
+				<ul>
+					<li>Presión constante.</li>
+					<li>Ahorro de energía.</li>
+					<li>Variador de velocidad enfriado por agua.</li>
+					<li>Instalación directa en la tubería.</li>
+					<li>Montaje en cualquier posición.</li>
+					<li>Diseño moderno y eficiente.</li>
+					<li>2 versiones: alimentación monofásica para bombas monofásicas y alimentación monofásica para bombas trifásicas.</li>
+					<li>Amplio rango de voltaje de entrada (170 a 270V).</li>
+					<li>Intuitivo.</li>
+					<li>Botones de navegación (+ y - ).</li>
+					<li>Display (muestra los valores de corriente, presión y alarmas de nuestro sistema de presión).</li>
+					<li>Operación con bombas de superficie o sumergibles (distancia máxima permitida sin filtro entre la bomba sumergible y el WATER DRIVE de 15 metros, para distancias mayores considerar la gama de filtros de HIDROCONTROL).</li>
+					<li>Ligero.</li>
+					<li>Robusto (succión y descarga de construcción metálica).</li>
+					<li>Puesta en Marcha fácil de realizar.</li>
+					<li>Led´s para programar y visualizar; presión del sistema, presión de re-arranque, corriente del motor y frecuencia de operación.</li>
+				</ul>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/WD-vector.gif" alt="vector">
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=08_FTHC-WTD-1309191.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		<a href="http://vde.com.mx/fichas-tecnicas/08_FTHC-WTD-1309191.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		<h2>WATER DRIVE ES INTEGRADO POR</h2>
+				<ul>
+					<li>Variador de velocidad.						 </li>
+					<li>Sensor de presión.							 </li>
+					<li>Relés de control y alarma.					 </li>
+					<li>Sensor de flujo.							 </li>
+					<li>Sensor de temperatura.						 </li>
+					<li>Display.									 </li>
+					<li>Botones de navegación.						 </li>
+					<li>Tubo galvanizado para la conexión hidráulica.</li>
+				</ul>
+	 		<h2>PROTECCIONES PARA LA MOTOBOMBA</h4>
+	 		<ul>
+	 			<li>Bajo voltaje.																											 </li>
+				<li>Alto voltaje.																											 </li>
+				<li>Sobrecarga.																												 </li>
+				<li>Corto circuito.																											 </li>
+				<li>Alta temperatura del líquido.																							 </li>
+				<li>Trabajo en seco.																										 </li>
+				<li>Alarma por golpe de ariete (es activada cuando se detecta un golpe de presión de 2 veces la presión de trabajo).		 </li>
+				<li>Alarma externa (está opción es utilizada para detener la operación del equipo en caso de colocar algún flotador externo).</li>
+		 	</ul>
+ 		</section>
+ 		<h2 class="tc">TABLA DE ESPECIFICACIONES</h4>
+		<img src="images/WD-tabla-04.gif" alt="tabla">
+ 		<h2>DIMENSIONES Y PESOS</h2>
+ 		<img src="images/WD-tabla-03.gif" alt="tabla" class="left">
+ 		<img src="images/WD-dimenciones.gif" alt="dimenciones" class="left ml1em">
+ 		<h2>DIMENSIONES Y PESOS</h2>
+ 		<img src="images/F-Drive_19.jpg" alt="Dimensiones y pesos" class="left mar-table">
+ 		<img src="images/F-Drive_21.jpg" alt="Vectores" class="left">
+		<div class="clear"></div>
+	</section>
+ 	
+ 	<?php include("iframe.php"); ?> 	
+ 	<?php include("footer.php"); ?>
