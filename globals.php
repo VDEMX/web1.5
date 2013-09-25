2a22db4b1ced3bf6a4ba80fa322cb3ff0e412112
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-09-25 14:47:49 -0500

    eliminé las carpetas de catálogo electrónico

--- globals.php
+++ globals.php
@@ -1,5 +1,5 @@
 <?php
 	$sitio = "Villarreal Divisi&oacute;n Equipos S.A. de C.V.";
-	$url = "http://vde.com.mx/";
+	$url = "http://localhost:8888/villarreal/";
 	$nombre = "VDE";
 ?>
--- serie-kor10.php
+++ serie-kor10.php
@@ -1,6 +1,6 @@
 <?php $active_page="productos"; ?>
 <?php include("head.php"); ?>
-<title>Sistemas Altamira | Serie KOR 10</title>
+ 		<title><?php echo "$nombre"?> | Serie KOR 10</title>
 <meta name="description" content="Estas bombas han sido diseñadas con tecnología de punta y construidas con materiales de la más alta calidad. En su fabricación se utiliza la maquinaria más moderna y precisa,logrando eficiencias hidráulicas sobresalientes que le garantizan bajos costos de operación.">
 <meta name="keywords" content="bombas, sumergibles, tecnología,calidad,moderna, precisa,eficiencias, hidráulicas, sistema, bombeo">
 <meta name="robots" content="Index, follow">
