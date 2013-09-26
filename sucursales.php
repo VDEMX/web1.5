311abac8ce4c8309f80401ace8ba4c6a3ee9f42f
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-09-26 16:16:11 -0500

    Mapa mejorado
    Implementación del api de google para el mapa de sucursales, oficinas
    centros de distribución a y atención al cliente.

--- sucursales.php
+++ sucursales.php
@@ -4,31 +4,15 @@
  		<meta name="description" content="Contamos con presencia a nivel nacional">
 		<meta name="keywords" content="diseñar, producir,distribuir,mayoreo, sistemas,equipos,alta calidad,agua, presencia, nacional, México, republica">
  		<meta name="robots" content="Index, follow">
- 		<script>
-var myLinks = document.getElementsByTagName('a');
-for(var i = 0; i < myLinks.length; i++){
-   myLinks[i].addEventListener('touchstart', function(){this.className = "hover";}, false);
-   myLinks[i].addEventListener('touchend', function(){this.className = "";}, false);
-}
- 		</script>
  	</head>
  	
  <body>
  <section id="container">
  	
  	<?php include("header.php"); ?>
- 	
- 	<section id="map" class="content active">
- 		<div id="tooltips">
-	 		<div id="tooltip-01"><a href="#"></a></div>	
-			<div id="tooltip-02"><a href="#"></a></div>	
-			<div id="tooltip-03"><a href="#"></a></div>	
-			<div id="tooltip-04"><a href="#"></a></div>	
-			<div id="tooltip-05"><a href="#"></a></div>	
-			<div id="tooltip-06"><a href="#"></a></div>
-			<div id="tooltip-07"><a href="#"></a></div>	
- 		</div>
-	</section>
+
+ 		<iframe id="map" class="content active" src="http://mapsengine.google.com/map/embed?mid=z_NQWtCVolDo.kK_Z6d7VjPDM" frameborder="0"></iframe>
+
  	
  	<?php include("iframe.php"); ?> 
  	<?php include("footer.php"); ?>
