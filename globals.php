e61c15af3a97302dda2700550ac4109475f04909
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-02 15:25:13 -0500

    Fotovoltaicos
    hice varios cambios en la sección de los fotovoltaicos arreglando
    algunos estilos, y arreglando unos botones de descarga de archivos

--- globals.php
+++ globals.php
@@ -1,5 +1,8 @@
 <?php
 	$sitio = "Villarreal Divisi&oacute;n Equipos S.A. de C.V.";
+	//Local
+	//$url = "http://localhost:8888/www.vde.com.mx/";
+	//FTP
 	$url = "http://vde.com.mx/";
 	$nombre = "VDE";
 ?>
