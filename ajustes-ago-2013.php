4a77f3fc275f0df7e196c3080b1345db983bc515
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-17 15:35:26 -0500

    2 nuevos avisos de cambios en la lista de precios

---
+++ ajustes-ago-2013.php
@@ -0,0 +1,58 @@
+<?php $active_page="federatas"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$nombre"?> | Ajustes Agosto 2013</title>
+ 		<meta name="description" content="informe debido a recientes incrementos de costos por parte de proveedores">
+		<meta name="keywords" content="informe, incrementos, costos, proveedores,Anexo, Fe de Erratas, correcciones, actualización, lista, precios, productos, cambios">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 	<?php include("header.php"); ?>	
+ 	<section id="federatas" class="active">
+			<h2 class="headline">Avisos importantes</h2>
+			<h3 class="titulo">Fecha de aviso: 22 de Agosto de 2013</h3>
+			<article>
+			<p><strong>Estimado Cliente:</strong><br><br>
+Anteponiendo un cordial saludo, aprovechamos para informarle que debido a incrementos de costos por parte de algunos de nuestros proveedores, nos vemos en la necesidad de realizar un ajuste de precios en ciertos productos tal como se menciona a continuación: <br><br>
+Los porcentajes de aumento indicados en esta tabla aplican sobre nuestra lista de precios 2012.</p>
+<table class="table">
+  <tr>
+    <th>Descripción</th>
+    <th>Ubicación dentro de la lista de precios VDE 2012</th>
+    <th>% de aumento</th>
+  </tr>
+  <tr>
+    <td>Tanques precargados ALTAMIRA  serie ACERO (color verde)</td>
+    <td>sección: 7 TANQUES PRECARGADOS<br />
+    página: 126</td>
+    <td rowspan="4" class="tc b">5%</td>
+  </tr>
+  <tr>
+    <td>Tanques precargados ALTAMIRA serie PRO</td>
+    <td>sección: 7 TANQUES PRECARGADOS<br />
+    página: 127</td>
+  </tr>
+  <tr>
+    <td>Presurizadores individuales ALTAMIRA con tanque ALTAMIRA serie ACERO (color verde)</td>
+    <td>sección: 8 PRESURIZADORES<br />
+    página: 131</td>
+  </tr>
+  <tr>
+    <td>Presurizadores individuales ALTAMIRA con tanque ALTAMIRA serie PRO</td>
+    <td>sección: 8 PRESURIZADORES<br />
+    página: 132</td>
+  </tr>
+</table>
+<p><strong>Aclaración:</strong> dicho aumento no aplica en los tanques precargados ALTAMIRA ProX y ProXL ni en los presurizadores con estos tanques. <br><br>
+
+Los ajustes mencionados entrarán en vigor a partir del lunes 2 de Septiembre de 2013.<br><br
+
+Para cualquier duda o comentario favor de comunicarse, con gusto le atenderemos.</p>
+			</article>
+			
+			<a href="/anexo" class="button"><span class="icon icon-atras"></span>Regresar</a>
+	</section>
+	
+ 	<?php include("iframe.php"); ?> 	
+ 	<?php include("footer.php"); ?>
---
+++ ajustes-sep-2013.php
@@ -0,0 +1,111 @@
+<?php $active_page="federatas"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$nombre"?> | Ajustes Septiembre 2013</title>
+ 		<meta name="description" content="informe debido a recientes incrementos de costos por parte de proveedores">
+		<meta name="keywords" content="informe, incrementos, costos, proveedores,Anexo, Fe de Erratas, correcciones, actualización, lista, precios, productos, cambios">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 	<?php include("header.php"); ?>	
+ 	<section id="federatas" class="active">
+			<h2 class="headline">Avisos importantes</h2>
+			<h3 class="titulo">Fecha de aviso: 28 de Septiembre de 2013</h3>
+			<article>
+			<p><strong>Estimado Cliente:</strong><br><br>
+Reciba un cordial saludo, así mismo hacemos de su conocimiento que debido a incrementos de costos por parte de algunos de nuestros proveedores, nos vemos en la necesidad de realizar un ajuste de precios en ciertos productos tal como se menciona a continuación: <br><br>
+Bombas Sumergibles 4” AQUA PAK Serie TASK <br>
+Ubicación: Lista de precios VDE 2012, sección 2 BOMBAS SUMERGIBLES, página 29.</p>
+<table class="table">
+  <tr>
+    <th>MODELO</th>
+    <th>POTENCIA (HP)</th>
+    <th>NUEVO PRECIO DE LISTA VDE (USD)</th>
+  </tr>
+  <tr>
+    <td>TASK05N-H</td>
+    <td class="tc">0.5</td>
+    <td class="tc">132</td>
+  </tr>
+  <tr>
+    <td>TASK10N-H</td>
+    <td class="tc">1</td>
+    <td class="tc">151</td>
+  </tr>
+  <tr>
+    <td>TASK10N-F</td>
+    <td class="tc">1</td>
+    <td class="tc">132</td>
+  </tr>
+  <tr>
+    <td>TASK15N-F</td>
+    <td class="tc">1.5</td>
+    <td class="tc">137</td>
+  </tr>
+</table>
+<p><strong>Aclaración:</strong> Las motobombas AQUA PAK serie TASK con motor sumergible FRANKLIN (Lista de precios VDE 2012 sección 2 página 29) no sufrieron cambio de precios. <br><br>
+
+Así mismo hacemos de su conocimiento que hemos hecho un ajuste de Precios de Lista A <strong>LA BAJA</strong>, tal como se indica en la siguiente tabla:<br><br>
+
+Bombas Sumergibles 4” AQUA PAK Serie MAX <br><br>
+Ubicación: Lista de precios VDE 2012, sección 2 BOMBAS SUMERGIBLES, páginas 26 y 27.</p>
+<table class="table">
+  <tr>
+    <th>MODELO</th>
+    <th>POTENCIA (HP)</th>
+    <th>NUEVO PRECIO DE LISTA VDE (USD)</th>
+  </tr>
+  <tr>
+    <td>M0.7X05-7</td>
+    <td class="tc">0.5</td>
+    <td class="tc">114</td>
+  </tr>
+  <tr>
+    <td>M0.7X10-12</td>
+    <td class="tc">1</td>
+    <td class="tc">147</td>
+  </tr>
+  <tr>
+    <td>M1X05-5</td>
+    <td class="tc">0.5</td>
+    <td class="tc">96</td>
+  </tr>
+  <tr>
+    <td>M1X10-10</td>
+    <td class="tc">1</td>
+    <td class="tc">131</td>
+  </tr>
+  <tr>
+    <td>M1X15-12</td>
+    <td class="tc">1.5</td>
+    <td class="tc">145</td>
+  </tr>
+  <tr>
+    <td>M1.5X05-5</td>
+    <td class="tc">0.5</td>
+    <td class="tc">106</td>
+  </tr>
+  <tr>
+    <td>M1.5X10-8</td>
+    <td class="tc">1</td>
+    <td class="tc">114</td>
+  </tr>
+  <tr>
+    <td>M1.5X15-11</td>
+    <td class="tc">1.5</td>
+    <td class="tc">119</td>
+  </tr>
+</table>
+<p>
+	Los ajustes mencionados entrarán en vigor a partir del martes 1° de octubre de 2013. <br><br>
+
+Para cualquier duda o comentario favor de comunicarse, con gusto le atenderemos.
+</p>
+			</article>
+			
+			<a href="/anexo" class="button"><span class="icon icon-atras"></span>Regresar</a>
+	</section>
+	
+ 	<?php include("iframe.php"); ?> 	
+ 	<?php include("footer.php"); ?>
