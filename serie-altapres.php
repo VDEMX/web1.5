0ed756eb23570d3da53856f77b08c5204b84f47e
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-18 12:19:11 -0500

    Agregar la serie Altapress

---
+++ serie-altapres.php
@@ -0,0 +1,93 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Altapres</title>
+ 		<meta name="description" content="La bomba marca Altamira serie ALTAPRES fue diseñada para aumentar la presión del agua y puede ser usada en múltiples aplicaciones, gracias a su sistema de impulsor flotante se logran máximas eficiencias, larga vida útil y un incomparable desempeño">
+		<meta name="keywords" content="bomba, Altamira, ALTAPRES, presión, agua, múltiples, aplicaciones, sistema, impulsor flotante, desempeño">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	 	<section id="ax" class="active">
+ 	 	<img src="images/altapres1-ficha-comercial.jpg" alt="Ficha comercial">
+ 	<div class="cien">
+ 	<h2 class="headline">Serie Altapres</h2>
+	 		<span>BOMBAS MULTIETAPAS HORIZONTALES PARA ALTA PRESIÓN</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<p>La bomba marca Altamira serie ALTAPRES fue diseñada para aumentar la presión del agua y puede ser usada en múltiples aplicaciones, gracias a su sistema de impulsor flotante se logran máximas eficiencias, larga vida útil y un incomparable desempeño.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Residencial, comercial o agrícola presión de lavado</li>
+					<li>Ósmosis inversa									   </li>
+					<li>Sistemas de filtración							   </li>
+					<li>Sistemas de Enfriamiento por rociado			   </li>
+					<li>Aumento de presión								   </li>
+					<li>Sistemas de circulación de agua					   </li>
+					<li>Climatización									   </li>
+					<li>Uso general de bombeo							   </li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li>Cuerpo de la bomba(tubo envolvente) : acero inoxidable AISI 304									 </li>
+					<li>Cabezal de descarga: hierro fundido																 </li>
+					<li>Soporte de motor: hierro fundido, con un robusto soporte de pie para montaje					 </li>
+					<li>Impulsores: tecnopolimero con carga de fibra de vidrio											 </li>
+					<li>Tazones: acero inoxidable AISI 304																 </li>
+					<li>Eje de la bomba: en acero inoxidable															 </li>
+					<li>Succión y descarga 1” son las mismas en toda la línea, simplifica la instalación y mantenimiento.</li>
+		 		</ul>
+		 		<h4>MOTOR</h4>
+		 		<ul>
+			 		<li>Asíncrono, dos polos, 60 Hz , 3,450 RPM										  </li>
+					<li>Motor abierto a prueba de goteo												  </li>
+					<li>Armazón NEMA 56J															  </li>
+					<li>Servicio continuo															  </li>
+					<li>Versión monofásica cuenta con protección contra baja carga (rearme automático)</li>
+					<li>Servicio continuo															  </li>
+		 		</ul>
+		 		<h2>CARACTERÍSTICAS DE OPERACIÓN</h2>
+		 		<ul>
+			 		<li>Temperatura máxima del fluido : 71°C	   </li>
+					<li>Presión máxima de succión (entrada): 75 PSI</li>
+		 		</ul>
+
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/altapres1-vector.gif" alt="Vector">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=07_FTAL-ALTAPRESI-1.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/07_FTAL-ALTAPRESI-1.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+		 	<h2>CÓDIGO DE LA BOMBA</h2>
+		 	<img src="images/altapres1-codigo.gif" alt="Vector">
+		 	<img src="images/altapres1.jpg" alt="Altapres">
+		 		
+ 		</section>
+ 		<h2>TABLA DE ESPECIFICACIONES</h2>
+ 		<img src="images/altapres1-tabla-02.gif" alt="Tabla">
+ 		<p class="nota">*ODP = Abierto a prueba de Goteo</p>
+ 		<img src="images/altapres1-tabla-01.gif" alt="Tabla">
+ 		<h2>Curvas de rendimiento</h2>
+ 		<h3>SERIE ALTAPRES 03</h3>
+ 		<img src="images/altapres1-curvas-03.gif" alt="Curva">
+ 		<h3>SERIE ALTAPRES 08</h3>
+ 		<img src="images/altapres1-curvas-02.gif" alt="Curva">
+ 		<h3>SERIE ALTAPRES 16</h3>
+ 		<img src="images/altapres1-curvas-01.gif" alt="Curva">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/altapres1-dimensiones-02.gif" alt="Dimensiones">
+ 		<img src="images/altapres1-dimensiones-01.gif" alt="Dimensiones">
+ 		<img src="images/altapres1-tabla-dimensiones.gif" alt="Dimensiones">
+ 		<p class="nota tc">Dimensiones en mm.</p>
+ 		
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?> 
+ 	<?php include("footer.php"); ?>
