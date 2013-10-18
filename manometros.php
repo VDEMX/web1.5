cb07bc9e78efe682f9b0d710a239f674572b7deb
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-18 13:01:19 -0500

    Agregar información de los manómetros

---
+++ manometros.php
@@ -0,0 +1,92 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Manómetros</title>
+ 		<meta name="description" content="Manómetros">
+		<meta name="keywords" content="<?php echo "$nombre"?>, Manómetros" >
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<h2 class="headline azul">Manómetros</h2>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>CARACTERÍSTICAS</h2>
+	 		<article>
+		 		<ul>
+					<li>Aprovado para bombas sumergibles ALTAMIRA, Franklin y nacionales.</li>
+		 		</ul>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Doméstico												 </li>
+					<li>Agrícola 												 </li>
+					<li>Ganadería 												 </li>
+					<li>Industria												 </li>
+					<li>Construcción 											 </li>
+					<li>Sistemas muicipales de agua potable 					 </li>
+					<li>Comerciales (hoteles, oficinas, cines, restaurantes, etc)</li>
+		 		</ul>
+		 		
+		 		
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/manometro.jpg" alt="Manómetro">
+ 		</section>
+ 		<h2>Especificaciones</h2>
+ 		<table class="table">
+  <tr>
+    <th scope="col">MODELO</th>
+    <th scope="col">DESCRIPCIÓN</th>
+    <th scope="col">RANGO DE PRESIÓN (psi)</th>
+    <th scope="col">CARÁTULA (PULG.)</th>
+    <th scope="col">CONEXIÓN HIDRÁULICA ROSCA MACHO</th>
+  </tr>
+  <tr>
+    <td>MAN0-100SS</td>
+    <td>Manómetro ALTAMIRA seco 0-100PSI A.INOX.</td>
+    <td rowspan="3">0-100</td>
+    <td>2</td>
+    <td rowspan="2">1/4&quot; NPT Base</td>
+  </tr>
+  <tr>
+    <td>MAN0-100G</td>
+    <td rowspan="2">Manómetro ALTAMIRA gliserina 0-100PSI A.INOX.</td>
+    <td rowspan="2">2.5</td>
+  </tr>
+  <tr>
+    <td>MAN0-100GBM</td>
+    <td>1/4&quot; NPT Trasera</td>
+  </tr>
+  <tr>
+    <td>MAN0-200SS</td>
+    <td>Manómetro ALTAMIRA seco 0-200PSI A.INOX.</td>
+    <td rowspan="3">0-200</td>
+    <td>2</td>
+    <td rowspan="2">1/4&quot; NPT Base</td>
+  </tr>
+  <tr>
+    <td>MAN0-200G</td>
+    <td rowspan="2">Manómetro ALTAMIRA gliserina 0-200PSI A.INOX.</td>
+    <td rowspan="3">2.5</td>
+  </tr>
+  <tr>
+    <td>MAN0-200GBM</td>
+    <td rowspan="2">1/4&quot; NPT Trasera</td>
+  </tr>
+  <tr>
+    <td>MAN0-300GBM</td>
+    <td>Manómetro ALTAMIRA gliserina 0-300PSI A.INOX.</td>
+    <td>0-300</td>
+  </tr>
+</table>
+ 		
+ 		<div class="clear"></div>
+	</section>
+<?php include("iframe.php"); ?> 
+<?php include("footer.php"); ?>
--- menu.php
+++ menu.php
@@ -112,8 +112,9 @@
          		</li>
          		<li class="boton"><a href="vde/medidormec.html" target="contenido"><span>Medidores de Flujo</span></a></li>
          		<li class="boton"><a href="vde/controlp.html" target="contenido"><span>Control Automático de Presión</span></a></li>
-         		<li class="boton"><a href="vde/accesorio_menores.html" target="contenido"><span>Válvulas, Manómetros, Switch de Presión, etc.</span></a></li>
+         		<li class="boton"><a href="vde/accesorio_menores.html" target="contenido"><span>Válvulas, Switch de Presión, etc.</span></a></li>
          		<li class="boton"><a href="/cable"><span>Cable Sumergible</span></a></li>
+         		<li class="boton"><a href="/manometro"><span>Manómetro</span></a></li>
          		<li class="boton"><a href="vde/arrancador.html" target="contenido"><span>Arrancadores Magnéticos</span></a></li>
          		<li class="boton"><a href="vde/arrancadors.html" target="contenido"><span>Arrancadores Suaves</span></a></li>
          		<li class="has-sub"><a href="#"><span>Protecciones para Motor</span></a>
