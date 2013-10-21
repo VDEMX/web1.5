347eb0d9f9c15c188c4afee848e6beb9ee2f5a84
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-21 17:31:05 -0500

    Actualizar la información de la Serie R

---
+++ serie-r.php
@@ -0,0 +1,74 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie R</title>
+ 		<meta name="description" content="El motor ALTAMIRA SERIE R es el resultado de una larga experiencia en motores sumergibles rebobinables a baño de agua, dando como resultado un motor de alta calidad. Su construcción robusta le permite trabajar por largo tiempo en condiciones normales, sin mantenimiento alguno.">
+		<meta name="keywords" content="motor, ALTAMIRA, SERIE R, motores, sumergibles, rebobinables, baño de agua, alta calidad">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	 	<section id="ax" class="active">
+ 	 	<img src="images/r-ficha-comercial.jpg" alt="Ficha Comercial">
+ 	<div class="cien">
+ 	<h2 class="headline morado">Serie R</h2>
+	 		<span>MOTORES SUMERGIBLES REBOBINABLES</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<p>El motor ALTAMIRA SERIE R es el resultado de una larga experiencia en motores sumergibles rebobinables a baño de agua, dando como resultado un motor de alta calidad. Su construcción robusta le permite trabajar por largo tiempo en condiciones normales, sin mantenimiento alguno.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Sistemas de agua potable</li>
+					<li>Ganadería				</li>
+					<li>Industrial				</li>
+					<li>Comercial				</li>
+					<li>Agrícola				</li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS Y VENTAJAS</h4>
+		 		<h5>Características de construcción</h5>
+		 		<p>Los motores rebobinables ALTAMIRA serie R son de una construcción particularmente robusta para trabajar por largo tiempo sin mantenimiento.</p>
+		 		<h5>Estator</h5>
+		 		<p>Bobinado realizado con alambre de cobre electrolítico revestido de material termoplástico impermeable al agua, con elevadas características dieléctricas. El motor puede trabajar con una tempertura máxima de 30°C, para operar con una tempertaura mayor se puede ordenar un motor con alambre especial . El bobinado de los devanados es con cabezas descubiertas, esto permite mayor facilidad para colocarlo y hacer más rápida y económica su reparación.</p>
+		 		<h5>Rotor tipo jaula de ardilla</h5>
+		 		<p>Eje rotor construido en acero inoxidable AISI 431 de complexión robusta, guiado por doble buje de grafito en el extremo superior y un buje sobredimensionado en el extremo inferior, soportado por un conjunto de empuje tipo Mitchell, con segmentos oscilantes en acero inoxidable y disco de grafito, dimensionado para resistir el máximo de empuje axial de la bomba con un elevado factor de seguridad.</p>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/r.jpg" alt="Vector">	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=02_FTAL-R-1.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/02_FTAL-R-1.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h3>Líquido refrigerante</h3>
+	 		<p>Los motores Altamira contienen una mezcla de agua y aceite no tóxico como refrigerante, la cual también tiene la función de lubricar y prevenir la oxidación. Si para alguna aplicación no está permitido el uso de estas sustancias los motores se pueden operar con agua limpia.</p>
+	 		<h3>Diafragma</h3>
+	 		<p>Está colocado en la parte inferior del motor para equilibrar la presión interna del mismo con la presión externa del pozo, y para la compensación de la variación del volumen del agua al calentarse durante el funcionamiento.</p>
+	 		<h3>Retenes</h3>
+	 		<p>Todos los motores Altamira de 6” de fabricación estándar son equipados con tres retenes colocados en la flecha en posición contrapuesta. De este modo se evita tanto la entrada como la salida de agua del motor.</p>
+	 		
+ 		</section>
+ 		<h2>TABLAS DE ESPECIFICACIONES</h2>
+ 		<h3 class="morado tc">MOTORES SUMERGIBLES DE 6” ALTAMIRA</h3>
+ 		<img src="images/r-tabla2.gif" alt="Tablas" class="mb1em">
+ 		<p class="nota"><strong>Nota:</strong> El motor de 50 HP viene con alambre para alta temperatura. (PE2 + PA).</p>
+ 		<h3 class="morado tc">MOTORES SUMERGIBLES DE 10” y 12”</h3>
+ 		<img src="images/r-tabla.gif" alt="Tablas" class="mb1em">
+ 		<p>Con la finalidad de contar con un buen sistema de protección, todos los motores ALTAMIRA R de 8” y 10”, se surten equipados con SENSOR de temperatura “PT100”. Por lo tanto, para dicho sistema de protección debe de incluir el receptor correspondiente, el cual se adquiere por separado.</p>
+ 		<section class="doscol">
+ 			<img src="images/k-tabla-1.gif" alt="tabla">	
+ 		</section>
+ 		<section class="doscol">
+ 			<img src="images/caja-mercury.jpg" alt="Caja">	
+ 		</section>
+ 		<h2>DIMENSIONES</h2>
+ 			<img src="images/r-dimensiones.gif" alt="tabla" class="mb1em">	
+ 		
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?> 
+ 	<?php include("footer.php"); ?>
