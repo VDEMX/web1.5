7fb7c04aadd315fcbce6c0f7718cce496c1474fd
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-21 12:49:09 -0500

    Actualizar la información de la serie Amiga

---
+++ serie-amiga.php
@@ -0,0 +1,74 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Amiga</title>
+ 		<meta name="description" content="Bomba sumergible muy compacta, totalmente fabricada en materiales plásticos, permite una manipulación muy fácil para cualquier tipo de aplicación doméstica.">
+		<meta name="keywords" content="Bomba, sumergible, compacta, materiales, plásticos, aplicación, doméstica.">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	 	<section id="ax" class="active">
+ 	 	<img src="images/amiga-ficha-comercial.jpg" alt="Ficha comercial">
+ 	<div class="cien">
+ 	<h2 class="headline azul">Serie Amiga</h2>
+	 		<span>BOMBA SUMERGIBLE PARA ACHIQUE</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<p>Bomba sumergible muy compacta, totalmente fabricada en materiales plásticos, permite una manipulación muy fácil para cualquier tipo de aplicación doméstica.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Desagüe de espacios inundados </li>
+					<li>Fuentes decorativas y cascadas </li>
+					<li>Vaciado de piscinas y depósitos</li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li>Cuerpo de la bomba: polipropileno con carga de fibra de vidrio.							 </li>
+					<li>Rejilla de aspiración y cubierta de descarga: polipropileno con carga de fibra de vidrio.</li>
+					<li>Cuerpo envolvente: polipropileno con carga de fibra de vidrio.							 </li>
+					<li>Eje de la bomba: acero inoxidable 410.													 </li>
+					<li>Impulsor: tecnopolímero ABS.															 </li>
+					<li>Impulsor abierto: tecnopolímero ABS con carga de fibra de vidrio.						 </li>
+					<li>Incluye adaptadores para manguera.														 </li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS DE OPERACIÓN</h4>
+		 		<ul>
+			 		<li>Temperatura máxima del fluido: 0°C - 40°C (30°F - 104°F).</li>
+					<li>Sumergencia máxima (m): 6								 </li>
+					<li>pH del líquido a bombear: 6 - 8 ppm.					 </li>
+		 		</ul>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/amiga-vector.gif" alt="Vector">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=10_FTAL-AMI-1307241.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/10_FTAL-AMI-1307241.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h2>MOTOR</h2>
+		 		<ul>
+			 		<li>Motor cerrado enfriado por aire.										  </li>
+					<li>Asincrónico, dos polos.													  </li>
+					<li>Protección IP 68.														  </li>
+					<li>Aislamiento clase F.													  </li>
+					<li>Servicio continuo.														  </li>
+					<li>Incluye 3 m de cable tomacorriente con clavija y además flotador de nivel.</li>
+		 		</ul>
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/amiga-curvas.gif" alt="Curvas" class="mb1em">
+ 		<img src="images/amiga-tabla.gif" alt="Tabla" class="mb1em">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/amiga-tabla-dimensiones.gif" alt="Tabla" class="mb1em">
+ 		<img src="images/amiga-dimensiones.gif" alt="dimensiones" class="mb1em">	
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
