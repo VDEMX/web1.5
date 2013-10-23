250761a12900957460e822d45cc49fd7d8a3ec95
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 15:57:45 -0500

    Actualizar la información de la serie Supra

---
+++ serie-supra.php
@@ -0,0 +1,77 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Supra</title>
+ 		<meta name="description" content="Las bombas SUPRA fueron diseñadas para recircular y realizar la filtración del agua en los sistemas de piscinas, sus altos estándares de calidad en materiales le permite ser una bomba muy atractiva y eficiente. Ideales para recircular el agua en su piscina">
+		<meta name="keywords" content="bombas, recircular, agua, piscina, alberca, filtración, agua, sistemas, piscinas">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/ficha-comercial-supra.jpg" alt="ficha comercial supra">
+ 	<h2 class="headline azul">Serie SUPRA</h2>
+	 		<span>SUPRA15,SUPRA20,SUPRA25 y SUPRA30</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>Las bombas SUPRA fueron diseñadas para recircular y realizar la filtración del agua en los sistemas de piscinas, sus altos estándares de calidad en materiales le permite ser una bomba muy atractiva y eficiente. Ideales para recircular el agua en su piscina.</p>
+				<h4>APLICACIONES</h4>
+				<p>Ideales para recircular el agua en su piscina.</p>
+				<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+				<ul>
+					<li>Cuerpo de la bomba y difusor en Polipropileno con carga de fibra de vidrio.		</li>
+					<li>Impulsor fabricado en tecnopolímero Noryl® (PPO con Carga de fibra de vidrio).	</li>
+					<li>Incluye pre-filtro (trampa de pelo) en Polipropileno.							</li>
+					<li>Tapa transparente en policarbonato.												</li>
+					<li>Eje motor y tornillería en acero inoxidable.									</li>
+					<li>Sello mecánico en carbón/ cerámica /NBR.										</li>
+					<li>Tapón de drenado de fácil acceso.												</li>
+					<li>Estator fabricado en aluminio.													</li>
+					<li>Conexiones de succión y descarga con rosca hembra y macho para facilitar la conexión, incluye racors de PVC para cementar en 2”.</li>
+</ul>
+<h4>CARACTERÍSTICAS DE OPERACIÓN</h4>
+				<ul>
+					<li>Rango de temperatura del fluido: 4°C a 50°C.</li>
+					<li>Autocebante a 1.5 m.c.a.					</li>
+					<li>Silenciosas 75 dB (decibeles).				</li>
+</ul>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/supra-vector.gif" alt="Supra">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=001-Serie_SUPRA.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/001-Serie_SUPRA.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+		 	<h4>MOTOR</h4>
+				<ul>
+					<li>Asíncrono, dos polos.										  </li>
+					<li>Protección IPX5.											  </li>
+					<li>Aislamiento clase F.										  </li>
+					<li>Servicio continuo.											  </li>
+					<li>La versión monofásica cuenta con 1.5 m de cable tomacorriente.</li>
+</ul>
+		 	<h2>Código de la bomba</h4>
+		 	<img src="images/supra-codigo.gif" alt="Código de la bomba">
+ 		</section>
+ 		<h2>DATOS TÉCNICOS</h2>
+ 		<img src="images/supra-tabla.gif" alt="supra-tabla">
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/supra-curvas.gif" alt="supra-tabla">
+ 		<img src="images/supra-gasto.gif" alt="supra-tabla">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/supra-dimensiones.gif" alt="supra-dimensiones">
+ 		<img src="images/supra-tabla-dimensiones.gif" alt="supra-tabla-dimensiones">
+ 		
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
