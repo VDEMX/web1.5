cc173f6fbf759422dac9e26a5f67216b9cbf5f70
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 11:17:48 -0500

    Actualizar la información de la serie FIX

---
+++ serie-fix.php
@@ -0,0 +1,74 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Fix</title>
+ 		<meta name="description" content="Bomba de superficie tipo JET, ideal para aplicaciones donde se requiere que el agua no tenga contacto con materiales sujetos a oxidación.">
+		<meta name="keywords" content="bombas, superficie, jet, tipo">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/fix-ficha-tecnica.jpg" alt="ficha comercial">
+ 	<h2 class="headline azul">Serie Fix</h2>
+	 		<span>BOMBAS DE SUPERFICIE (TIPO JET)</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>Bomba de superficie tipo JET, ideal para aplicaciones donde se requiere que el agua no tenga contacto con materiales sujetos a oxidación.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<p>
+			 		Las bombas serie FIX marca AQUA PAK son ideales para: sistemas hidroneumáticos, riego por aspersión, llenado de depósitos (tinacos), etc.
+		 		</p>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li><strong>Cuerpo de la bomba:</strong> en acero inoxidable AISI 304. </li>
+					<li><strong>Impulsor y difusor:</strong> en tecnopolímero Noryl®( PPO).</li>
+					<li><strong>Eje de la bomba:</strong> en acero inoxidable AISI 416.	 </li>
+					<li><strong>Sello mecánico:</strong> Cerámica/Carbón.				 </li>
+					<li><strong>Estator:</strong> en aluminio.							 </li>
+		 		</ul>
+		 		<h4>MOTOR</h4>
+		 		<ul>
+			 		<li>Motor cerrado enfriado por aire.		  </li>
+					<li>Asíncrono, dos polos.					  </li>
+					<li>Protección IP 45.						  </li>
+					<li>Aislamiento clase F.					  </li>
+					<li>Servicio continuo.						  </li>
+					<li>1.5 m. de cable tomacorriente con clavija.</li>
+					<li>Interruptor encendido/apagado integrado.  </li>
+		 		</ul>
+		 	
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/fix-vector.jpg" alt="Vector">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=04_FTAQ-FIX-1307111.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/04_FTAQ-FIX-1307111.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h2>CARACTERÍSTICAS DE OPERACIÓN</h2>
+	 		<ul>
+		 		<li>Rango de temperatura del líquido a bombear: 4°C a 35°C.</li>
+		 		<li>Autoaspirante a 9 mca (a nivel del mar).			   </li>
+	 		</ul>
+	 		<h2>CÓDIGO DE LA BOMBA</h2>
+	 		<img src="images/fix-codigo.jpg" alt="código">
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/fix-curvas.jpg" alt="curvas">
+ 		<img src="images/fix-tabla.jpg" alt="especificaciones">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/fix-dimensiones.jpg" alt="dimensiones">
+ 		<img src="images/fix-tabla-dimensiones.jpg" alt="dimensiones">
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
