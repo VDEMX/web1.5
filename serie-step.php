52e6fe86ae8ab873007a5345a64dc376b68e194c
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 15:42:45 -0500

    Actualizar la información de la serie Step

---
+++ serie-step.php
@@ -0,0 +1,77 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Step</title>
+ 		<meta name="description" content="La bomba multietapas serie STEP marca aqua pak nos ofrece confiabilidad y un bajo consumo energético. Es ideal para el uso doméstico , especialmente para la presurización y sistemas de irrigación.">
+		<meta name="keywords" content="bombas, multietapas, confiabilidad, bajo consumo, uso domestico, presurización, irrigación ">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/step-ficha-comercial.jpg" alt="ficha comercial">
+ 	<h2 class="headline azul">Serie Step</h2>
+	 		<span>BOMBAS CENTRÍFUGAS MULTIETAPAS HORIZONTALES</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>La bomba multietapas serie STEP marca aqua pak nos ofrece confiabilidad y un bajo consumo energético. Es ideal para el uso doméstico , especialmente para la presurización y sistemas de irrigación.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Suministro de agua a viviendas</li>
+					<li>Riego a jardines			  </li>
+					<li>Sistemas hidroneumáticos	  </li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li>Camisa(tubo envolvente) en acero inoxidable 304</li>
+					<li>Succión y descarga en hierro fundido		   </li>
+					<li>Impulsores y difusores fabricados en Noryl®	   </li>
+					<li>Eje de la bomba: en acero inoxidable 304	   </li>
+					<li>Sello mecánico de carbón/cerámica			   </li>
+					<li>Tapón de purga y cebado en Bronce			   </li>
+					<li>Carcasa de motor en aluminio				   </li>
+		 		</ul>
+		 		<h4>MOTOR</h4>
+		 		<ul>
+			 		<li>Asíncrono, dos polos		  </li>
+					<li>Protección IP 44			  </li>
+					<li>Aislamiento clase F			  </li>
+					<li>Servicio continuo			  </li>
+					<li>Protección térmica incorporado</li>
+					<li>Interruptor a prueba de goteo </li>
+		 		</ul>
+		 	
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/step.jpg" alt="Vector">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=04_FTAQ-STEP-1.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/04_FTAQ-STEP-1.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h2>CARACTERÍSTICAS DE OPERACIÓN</h2>
+	 		<ul>
+		 		<li>Temperatura máxima del fluido : 35°C</li>
+		 		<li>Autoaspirante a 2 m.c.a.			</li>
+	 		</ul>
+	 		<h2>CÓDIGO DE LA BOMBA</h2>
+	 		<img src="images/step-codigo.jpg" alt="código">
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/step-curvas.jpg" alt="curvas">
+ 		<img src="images/step-tabla.jpg" alt="especificaciones">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/step-dimensiones.jpg" alt="dimensiones">
+ 		<img src="images/step-tabla-dimensiones.jpg" alt="dimensiones">
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
