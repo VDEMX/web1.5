a99d0bd3559a6a8b0399ccd978c70c96c19882f8
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 12:58:57 -0500

    Actualizar la información de la serie Sigma

---
+++ serie-silver.php
@@ -0,0 +1,74 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie silver</title>
+ 		<meta name="description" content="Bombas centrífugas monoetapas autoaspirantes para la filtración del agua de piscinas, con prefiltro incorporado. Muy silenciosas">
+		<meta name="keywords" content="bombas, centrífugas,monoetapas, autoaspirantes, piscina, alberca, filtración, agua, sistemas, piscinas">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/silver-ficha-tecnica.jpg" alt="ficha comercial Silver">
+ 	<h2 class="headline azul">Serie SILVER</h2>
+	 		<span>SILVER7,SILVER10,SILVER12 y SILVER15</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>Bombas centrífugas monoetapas autoaspirantes para la filtración del agua de piscinas, con prefiltro incorporado. Muy silenciosas.</p>
+				<h4>APLICACIONES</h4>
+				<p>Las bombas AQUA PAK Serie SILVER son ideales para recircular el agua en su piscina.</p>
+				<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+				<ul>
+					<li>Cuentan con pre-filtro (trampa de pelo) con tapa transparente.</li>
+					<li>Fabricadas con materiales anticorrosivos, eje motor y tornillería en acero inoxidable, impulsores y difusores en materiales plásticos de alta calidad.</li>
+					<li>Conexiones de succión y descarga con rosca hembra y macho para facilitar la conexión, incluye racors para cementar en 1.5”.</li>
+					<li>Muy compactas.</li>
+					<li>Excelente eficiencia.</li>
+					<li>Silenciosas 73 dB (decibeles).</li>
+					<li>Tapón de drenado de fácil acceso.</li>
+</ul>
+<h4>CARACTERÍSTICAS DE OPERACIÓN</h4>
+				<ul>
+					<li>Temperatura máxima del fluido: 35°C.</li>
+					<li>Autoaspirante a 3 m.c.a.			</li>
+				</ul>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/supra-vector.gif" alt="Supra">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=001-Serie_SILVER.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/001-Serie_SILVER.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+		 	<h4>MOTOR</h4>
+				<ul>
+					<li>Asíncrono, dos polos.													</li>
+					<li>Protección IPX5.														</li>
+					<li>Aislamiento clase F.													</li>
+					<li>Servicio continuo.														</li>
+					<li>Con protector térmico incorporado.										</li>
+					<li>Con 1.4 m de cable tomacorriente, los modelos en 115 V incluyen clavija.</li>
+</ul>
+		 	<h2>Código de la bomba</h4>
+		 	<img src="images/silver-codigo.gif" alt="Código de la bomba">
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/silver-curvas.gif" alt="CURVAS DE RENDIMIENTO">
+ 		<img src="images/silver-especificaciones.gif" alt="Especificaciones">
+ 		<p class="small"><strong>Nota importante:</strong> con el objetivo de conseguir un filtrado más eficiente y ahorro de energía, le sugerimos dimensionar la bomba para su piscina, tomando un tiempo de filtrado mayor a 10 hrs. De esta manera resultará un equipo de menor potencia con un flujo laminar y más uniforme, además contribuye a tener una menor carga instalada (kw) y así cuidar la tarifa con la que se le factura el consumo eléctrico.</p>
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/silver-dimensiones.gif" alt="silver-dimensiones">
+ 		<img src="images/silver-tabla-dimenciones.gif" alt="silver-tabla-dimensiones">
+ 		
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
