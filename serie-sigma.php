bafb6bcb6245161c8c4b4d1eb2aabcc204dd4cf5
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 12:34:04 -0500

    Actualizar la información de la serie Sigma

---
+++ serie-sigma.php
@@ -0,0 +1,80 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie SIGMA</title>
+ 		<meta name="description" content="Bombas sumergibles para achique">
+		<meta name="keywords" content="bombas, sumergibles, achique, lodos,Desagüe, Vaciado, Fuentes decorativas, cascadas">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<h2 class="headline lodo">Serie SIGMA</h2>
+	 		<span>54A,110A,155AX,160A y 300A</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>Bombas sumergibles para achique.</p>
+				<h4>APLICACIONES</h4>
+				<ul>
+					<li>Desagüe de espacios inundados como sótanos, edificios, etc.</li>
+					<li>Vaciado de piscinas y depósitos.</li>
+					<li>Fuentes decorativas, cascadas, etc.</li>
+				</ul>
+				<h4>CARACTERÍSTICAS</h4>
+				<ul>
+					<li>Las bombas AQUA PAK serie SIGMA cuentan con interruptor flotador que les sirve como protección para el motor. La longitud del cable del interruptor flotador es de 40 cm.</li>
+					<li>Fabricadas con partes de alta tecnología para una larga duración.</li>
+					<li>Aspiración de nivel de agua hasta 3 mm (modelos 54A y 110A).</li>
+					<li>Protector de sobrecarga térmico incluido.</li>
+					<li>Incluye 3 m de cable de alimentación con clavija.</li>
+					<li>Descarga con rosca NPT.</li>
+					<li>Adaptador para manguera incluido.</li>
+					<li>Diseño de impulsor abierto, solamente la bomba SIGMA 155AX cuenta con impulsor tipo Vortex.</li>
+</ul>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/sigma.jpg" alt="Sigma">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=004_Serie_SIGMA.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/004_Serie_SIGMA.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+		 	<h2>Código de la bomba</h4>
+		 	<img src="images/codigo-sigma.gif" alt="Código de la bomba">
+ 		</section>
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/sigma-dimensiones.gif" alt="Tabla de dimensiones" class="left m1em">
+ 		<img src="images/sigma-dimvector.gif" alt="Dimensiones" class="left m1em">
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/sigma-curvas.gif" alt="Curvas de rendimiento">
+ 		<img src="images/sigma-espesificaciones.gif" alt="Especificaciones">
+ 		<h2 class="left">DESPIECE SIGMA</h2>
+ 		<img src="images/sigma54a.gif" alt="Sigma 54A" class="left m1em p30">
+ 		<img src="images/sigma54a-tabla.gif" alt="Sigma 54A" class="left m1em">
+ 		<img src="images/sigma110a.gif" alt="Sigma 110A" class="left m1em p30">
+ 		<img src="images/sigma110a-tabla.gif" alt="Sigma 110A" class="left m1em">
+ 		<img src="images/sigma155ax.gif" alt="Sigma 155AX" class="left m1em p30">
+ 		<img src="images/sigma155ax-tabla.gif" alt="Sigma 155AX" class="left m1em mb9em">
+ 		<img src="images/sigma160a.gif" alt="Sigma 160A" class="left m1em p30">
+ 		<img src="images/sigma160a-tabla.gif" alt="Sigma 160A" class="left m1em">
+ 		<img src="images/sigma300a.gif" alt="Sigma 300A" class="left m1em p30">
+ 		<img src="images/sigma300a-tabla.gif" alt="Sigma 300A" class="left m1em">
+		<h6>Notas:</h6>
+		<p class="small">
+		<strong>PP + GF</strong> = Polipropileno más carga de fibra de vidrio.<br>
+<strong>Sello Mecánico CA/CE</strong> = Resorte en acero inoxidable y caras de carbón/ cerámica.<br>
+<strong>ADC 12</strong> = Rodamiento de bola fabricado en aleación de aluminio de alta resistencia con contacto angular de alta precisión.<br>
+<strong>CNM5K</strong> = Rodamiento de bola fabricado en acero con juego radial interno dealta precisión.
+		</p>
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
