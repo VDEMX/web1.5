ba2a60f17e078ffd0bd34fe3a8a15e1ced1b6e4a
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 11:11:58 -0500

    Actualizar la información de la serie AP

---
+++ serie-ap.php
@@ -0,0 +1,78 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie AP</title>
+ 		<meta name="description" content="La bomba de superficie serie AP cuenta con impulsor de álabes periféricos de flujo radial, es ideal para bombear agua limpia, sin partículas abrasivas o líquidos químicamente no agresivos.">
+		<meta name="keywords" content="bombas, superficie, álabes, agua limpia, líquidos">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>
+ 	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/serie-ap-ficha-comercial.jpg" alt="ficha comercial">
+ 	<h2 class="headline azul">Serie AP</h2>
+	 		<span>Bombas periféricas</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>La bomba de superficie serie AP cuenta con impulsor de álabes periféricos de flujo radial, es ideal para bombear agua limpia, sin partículas abrasivas o líquidos químicamente no agresivos.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<ul>
+			 		<li>Para trabajar con aguas limpias		   </li>
+					<li>Suministro de agua en casa-habitación  </li>
+					<li>Para pequeños conjuntos hidroneumáticos</li>
+					<li>Para subir agua al tinaco			   </li>
+		 		</ul>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li><strong>Cuerpo de la bomba:</strong> Hierro fundido					 </li>
+					<li><strong>Impulsor:</strong> Aleación de bronce, con álabes periféricos radiales.	</li>
+					<li><strong>Eje de la bomba (AP-4):</strong> en acero con terminado en cromo</li>
+					<li><strong>Eje de la bomba (AP-5X):</strong> en acero inoxidable AISI 416</li>
+					<li><strong>Sello mecánico:</strong> Cerámica y grafito.					 </li>
+		 		</ul>
+		 		<h5>Ventajas adicionales de la AP-5X</h5>
+		 		<p>Cuerpo de la bomba: Hierro fundido con inserto antibloqueo en acero inoxidable. Cuenta con interruptor a prueba de goteo, cable tomacorriente y clavija.</p>
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/serie-ap-vectores.jpg" alt="Vectores">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=04_FTAQ-AP-1.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/04_FTAQ-AP-1.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h2>MOTOR</h2>
+	 		<ul>
+		 		<li>Motor cerrado, enfriado por aire.</li>
+				<li> Asincrónico, dos polos.		</li>
+				<li> Protección IP 44.				</li>
+				<li> Aislamiento clase B.			</li>
+				<li> Servicio continuo.				</li>
+				<li>Protección térmica incorporada.	</li>
+	 		</ul>
+	 		<h2>CARACTERÍSTICAS DE OPERACIÓN</h2>
+	 		<ul>
+		 		<li>Autoaspirante a 3 m.								</li>
+		 		<li>Temperatura del líquido (°C): Min: 10 °C hasta 60 °C</li>
+	 		</ul>
+
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<img src="images/serie-ap-curvas.jpg" alt="curvas">
+ 		<img src="images/serie-ap-tabla-especificaciones.gif" alt="especificaciones">
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/serie-ap4-dimensiones.gif" alt="dimensiones">
+ 		<img src="images/serie-ap4-dimensiones.jpg" alt="dimensiones">
+ 		<img src="images/serie-ap-dimensiones.jpg" alt="dimensiones">
+ 		<img src="images/serie-ap5-dimensiones.jpg" alt="dimensiones">
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
