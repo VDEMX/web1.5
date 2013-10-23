8c0b382d77df2d59c0f6ee29238bcc4a7deb4a4d
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2013-10-23 10:46:44 -0500

    Agregar la información de la serie Aly

---
+++ serie-aly.php
@@ -0,0 +1,79 @@
+<?php $active_page="productos"; ?>
+<?php include("head.php"); ?>
+ 		<title><?php echo "$sitio"?> | Serie Aly</title>
+ 		<meta name="description" content="Bomba de superficie tipo JET, ideal para aplicaciones donde se requiere que el agua no tenga contacto con materiales sujetos a oxidación.">
+		<meta name="keywords" content="bombas, superficie, tipo JET, hidroneumáticos, riego, aspersión, depósitos,tinacos">
+ 		<meta name="robots" content="Index, follow">
+ 	</head>
+ 	
+ <body>
+ <section id="container">
+ 
+ 	<?php include("header.php"); ?>	
+ 	<section id="ax" class="active">
+ 	<div class="cien">
+ 	<img src="images/aly-ficha-comercial.jpg" alt="ficha comercial">
+ 	<h2 class="headline azul">Serie Aly</h2>
+	 		<span>BOMBAS DE SUPERFICIE (TIPO JET)</span>
+ 	</div>
+ 		<section class="doscol">
+	 		<h2>ESPECIFICACIONES</h2>
+	 		<article>
+		 		<h4>DESCRIPCIÓN</h4>
+		 		<p>Bomba de superficie tipo JET, ideal para aplicaciones donde se requiere que el agua no tenga contacto con materiales sujetos a oxidación.</p>
+		 		<h4>APLICACIONES</h4>
+		 		<p>
+			 		Las bombas serie ALY marca AQUA PAK son ideales para: sistemas hidroneumáticos, riego por aspersión, llenado de depósitos (tinacos), etc.
+		 		</p>
+		 		<h4>CARACTERÍSTICAS DE CONSTRUCCIÓN</h4>
+		 		<ul>
+			 		<li><strong>Cuerpo de la bomba:</strong> en acero inoxidable AISI 304. </li>
+					<li><strong>Impulsor y difusor:</strong> en tecnopolímero Noryl®( PPO).</li>
+					<li><strong>Eje de la bomba:</strong> en acero inoxidable AISI 416.	 </li>
+					<li><strong>Sello mecánico:</strong> Cerámica/Carbón.				 </li>
+					<li><strong>Estator:</strong> en aluminio.							 </li>
+		 		</ul>
+		 		<h4>MOTOR</h4>
+		 		<ul>
+			 		<li>Motor cerrado enfriado por aire.		  </li>
+					<li>Asíncrono, dos polos.					  </li>
+					<li>Protección IP 45.						  </li>
+					<li>Aislamiento clase F.					  </li>
+					<li>Servicio continuo.						  </li>
+					<li>1.5 m. de cable tomacorriente con clavija.</li>
+					<li>Interruptor encendido/apagado integrado.  </li>
+		 		</ul>
+		 	
+	 		</article>
+ 		</section>
+ 		<section class="doscol">
+	 		<img src="images/aly-vector.gif" alt="Vector">
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/download.php?file=04_FTAQ-ALY-1307041.pdf"><button type="button" class="button"> <div class="icon icon-descargar"></div> Descargar ficha técnica</button></a>
+	 		
+	 		<a href="http://vde.com.mx/fichas-tecnicas/04_FTAQ-ALY-1307041.pdf" target="_blank"><button type="button" class="button"> <div class="icon icon-ojo"></div>Visualizar ficha técnica</button></a>
+	 		
+	 		<h2>CARACTERÍSTICAS DE OPERACIÓN</h2>
+	 		<ul>
+		 		<li>Rango de temperatura del líquido a bombear: 4°C a 35°C</li>
+		 		<li>Autoaspirante a 9 mca (a nivel del mar).			  </li>
+	 		</ul>
+	 		<h2>CÓDIGO DE LA BOMBA</h2>
+	 		<img src="images/aly-codigo.jpg" alt="código">
+ 		</section>
+ 		<h2>CURVAS DE RENDIMIENTO</h2>
+ 		<h3 class="azul-aqua">ALY15, ALY 20, ALY 30 Y ALY 40</h3>
+ 		<img src="images/aly-curvas.gif" alt="curvas">
+ 		<h2>CURVA DE RENDIMIENTO</h2>
+ 		<h3 class="azul-aqua">ALY05</h3>
+ 		<img src="images/aly-curva.jpg" alt="curvas">
+ 		<img src="images/aly-tabla.jpg" alt="especificaciones">
+ 		
+ 
+ 		<h2>DIMENSIONES</h2>
+ 		<img src="images/aly-dimensiones.jpg" alt="dimensiones">
+ 		<img src="images/aly-tabla-dimensiones.jpg" alt="dimensiones">
+		<div class="clear"></div>
+	</section>
+ 	<?php include("iframe.php"); ?>
+ 	<?php include("footer.php"); ?>
